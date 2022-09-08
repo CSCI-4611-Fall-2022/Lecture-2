9e037c04207bbc1638f5cdf4efec700ce2800733
Evan Suma Rosenberg <suma@umn.edu>
2022-09-08 16:45:12 -0500

    example rectangle

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -2,19 +2,23 @@ import * as gfx from './GopherGfx'
 
 export class ExampleApp extends gfx.GfxApp
 {
+    private exampleRectangle: gfx.Rectangle;
 
     constructor()
     {
         super();
+
+        this.exampleRectangle = new gfx.Rectangle();
     }
 
     createScene(): void 
     {
-       
+       this.exampleRectangle.scale.y = 0.1;
+       this.scene.add(this.exampleRectangle);
     }
 
     update(deltaTime: number): void 
     {
-       
+        this.exampleRectangle.position.y += 0.01;
     }
 }
