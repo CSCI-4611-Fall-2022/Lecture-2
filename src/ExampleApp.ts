ec51712e14ef37b4463dcc44593d24916741ddad
Evan Suma Rosenberg <suma@umn.edu>
2022-09-08 16:58:14 -0500

    class changes

--- src/ExampleApp.ts
+++ src/ExampleApp.ts
@@ -9,16 +9,46 @@ export class ExampleApp extends gfx.GfxApp
         super();
 
         this.exampleRectangle = new gfx.Rectangle();
+
+        let i = 5;
+        i = 10;
     }
 
     createScene(): void 
     {
        this.exampleRectangle.scale.y = 0.1;
        this.scene.add(this.exampleRectangle);
+
+       
+
+       const words = ['hi1', 'bye1'];
+       for(let i=0; i < words.length; i++)
+       {
+         console.log(words[i]);
+       }
+
+       const emptyArray: string[] = [];
+       emptyArray.push('hi2');
+       emptyArray.push('bye2');
+
+       emptyArray.forEach((elem: string) => {
+            console.log(elem);
+       });
+
     }
 
     update(deltaTime: number): void 
     {
         this.exampleRectangle.position.y += 0.01;
+        //this.printHello();
+    }
+
+    private printHello(): void
+    {
+        //console.log('Hello');
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        console.log('mouse down ' + event.x + ' ' + event.y);
     }
 }
